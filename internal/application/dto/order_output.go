01d9751ce147b24d1505772eb545d8decb1fa7b3
Kauan Carvalho Calasans <kauan.carvalho.calasans@gmail.com>
2025-05-12 22:18:04 -0300

    feat: hidden field when not mapped

--- internal/application/dto/order_output.go
+++ internal/application/dto/order_output.go
@@ -3,9 +3,9 @@ package dto
 import "time"
 
 type PaymentOutput struct {
-	Status        string `json:"status"`
-	QRCode        string `json:"qr_code"`
-	PaymentMethod string `json:"payment_method"`
+	Status        string `json:"status,omitempty"`
+	QRCode        string `json:"qr_code,omitempty"`
+	PaymentMethod string `json:"payment_method,omitempty"`
 }
 
 type OrderOutput struct {
@@ -16,5 +16,5 @@ type OrderOutput struct {
 	CreatedAt time.Time         `json:"created_at"`
 	UpdatedAt time.Time         `json:"updated_at"`
 	Items     []OrderItemOutput `json:"items"`
-	Payment   PaymentOutput     `json:"payment"`
+	Payment   PaymentOutput     `json:"payment,omitempty"`
 }
